7a86abb1f2931797f539083acfe7f976a47b8dde
ryan coughlan <rcoughlan90@gmail.com>
2019-08-20 15:44:42 +0100

    updating prod env

--- src/components/Login.js
+++ src/components/Login.js
@@ -10,7 +10,11 @@ const Login = () => {
         <Row>
             <Col></Col>
             <Col>
-                <Button className="login-button" variant="success" size="lg" onClick={() => window.location = 'http://localhost:8888/login'}>Sign Into Spotify</Button>
+                <Button className="login-button" variant="success" size="lg" onClick={() => {
+                    window.location = window.location.includes('localhost')
+                        ? 'http://localhost:8888/login'
+                        : 'https://spotifyrouter.herokuapp.com/login'
+                }}>Sign Into Spotify</Button>
             </Col>
             <Col></Col>
         </Row>
