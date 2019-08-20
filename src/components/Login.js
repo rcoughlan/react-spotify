98b6ee58660d1452583666289a847ff66ce37d53
ryan coughlan <rcoughlan90@gmail.com>
2019-08-20 15:48:29 +0100

    updating prod env

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,17 +4,18 @@ import {
     Col,
     Row
 } from 'react-bootstrap';
+import { SSL_OP_NO_TLSv1_1 } from 'constants';
 
 const Login = () => {
     return (
         <Row>
             <Col></Col>
             <Col>
-                <Button className="login-button" variant="success" size="lg" onClick={() => {
-                    window.location = window.location.includes('localhost')
+                <Button className="login-button" variant="success" size="lg" onClick={() => 
+                    window.location = (window.location.href.indexOf('localhost') > -1)
                         ? 'http://localhost:8888/login'
                         : 'https://spotifyrouter.herokuapp.com/login'
-                }}>Sign Into Spotify</Button>
+                }>Sign Into Spotify</Button>
             </Col>
             <Col></Col>
         </Row>
